82b75f2075e23a0bdbfac7219e42d8ebc23b068f
Nicolas Brown <nclsbrwn@gmail.com>
2021-04-05 22:55:39 -0500

    set container defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ui: {
+          background: 'var(--color-ui-background)',
+          sidebar: 'var(--color-ui-sidebar)',
+          typo: 'var(--color-ui-typo)',
+          primary: 'var(--color-ui-primary)',
+          border: 'var(--color-ui-border)'
+        }
+      },
+      spacing: {
+        sm: '24rem'
+      },
+      screens: {
+        "2xl": '1400px'
+      }
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    }
   },
   variants: {
     extend: {},
