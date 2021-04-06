c75a6aa4e935a1e43905595dad4d51f16a66393f
Nicolas Brown <nclsbrwn@gmail.com>
2021-04-05 22:56:03 -0500

    complete basic nav layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
-<div class="flex justify-between flex-row text-center">
-    <div><g-link to="/">TextureLab</g-link></div>
-    <div>
-        <g-link class="text-lg px-4" to="/docs">Docs</g-link>
-        <g-link class="text-lg px-4" to="/blog">Blog</g-link>
-        <g-link class="text-lg px-4" to="/download">Download</g-link>
-        <g-link class="text-lg px-4" to="/github">Github</g-link>
+<div class="bg-gray-800 text-white">
+    <div class="container mx-auto">
+        <div class="flex justify-between text-center items-center">
+            <div><g-link class="inline-block text-lg px-4 py-4" to="/">TextureLab</g-link></div>
+            <div>
+                <g-link class="inline-block text-lg px-4 py-4" to="/docs">Docs</g-link>
+                <g-link class="inline-block text-lg px-4 py-4" to="/blog">Blog</g-link>
+                <g-link class="inline-block text-lg px-4 py-4" to="/download">Download</g-link>
+                <g-link class="inline-block text-lg px-4 py-4" to="/github">Github</g-link>
+            </div>
+        </div>
     </div>
 </div>
 </template>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <div class="w-full">
     <Header />
     <slot/>
   </div>
