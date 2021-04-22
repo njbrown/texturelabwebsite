2cacbbe854080ac6dfd80cd0d81fab0b2f2da777
Nicolas Brown <nclsbrwn@gmail.com>
2021-04-21 21:27:23 -0500

    add barlow font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 import "@fontsource/inter"
+import "@fontsource/barlow"
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
