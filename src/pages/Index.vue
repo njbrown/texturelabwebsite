f36d0595d6505742a558b3dc73bc9f8e8b2a1211
Nicolas Brown <nclsbrwn@gmail.com>
2021-04-21 21:39:51 -0500

    add get started button on homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,7 +6,8 @@
           <h1 class="text-5xl pt-8 font-bold mx-auto max-w-4xl font-barlow">Procedural Texturing is the Future</h1>
           <h4 class="text-2xl mt-4 text-gray-300 mx-auto max-w-lg">With TextureLab, you dont have to be an artist to make beautiful textures</h4>
           <div>  
-            <a class="px-5 py-5 mt-8 mb-12 rounded bg-blue-400 text-2xl font-bold inline-block flex-wrap" href="#">Download</a>
+            <a class="px-5 py-5 mt-8 mb-12 rounded bg-blue-400 text-2xl font-bold inline-block flex-wrap" href="#" target="_blank">Download</a>
+            <a class="px-5 py-5 mt-8 mb-12 rounded bg-white text-gray-600 text-2xl font-bold inline-block flex-wrap ml-8" href="#">Get Started</a>
           </div>
           <!-- <g-image class="mt-8 -mb-20 shadow-xl" src="../../static/images/screenshot.png" /> -->
         </div>
