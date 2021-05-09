52c3b576f81286296471d5d66acdbb1131b41314
Nicolas Brown <nclsbrwn@gmail.com>
2021-05-09 01:12:37 -0500

    change favicon and add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 <div class="bg-gray-900 text-white">
     <div class="container mx-auto">
         <div class="flex justify-between text-center items-center">
-            <div><g-link class="inline-block text-lg px-4 py-4" to="/">TextureLab</g-link></div>
+            <div><g-link class="inline-block text-lg px-4 py-4" to="/"><img style="height:1.3em;display:inline-block;margin-right:0.2em" src="../../static/images/logo.png" />TextureLab</g-link></div>
             <div>
                 <!-- <g-link class="inline-block text-lg px-4 py-4" to="/blog">Blog</g-link> -->
                 <a class="md:inline-block text-lg px-4 py-4 hidden md:visible" href="https://njbrown.itch.io/texturelab" target="_blank">Download</a>
