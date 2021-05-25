32c15b8e8fabf3a092fc2252e3f13faeae7070fe
Nicolas Brown <nclsbrwn@gmail.com>
2021-05-24 22:58:44 -0500

    add basic layout for texture page

--- src/templates/GalleryPage.vue
+++ src/templates/GalleryPage.vue
@@ -2,31 +2,68 @@
   <Layout>
     <div class="w-full pt-8 pb-8 bg-gray-100">
       <div 
-        class="container text-black text-center text-black max-w-4xl"
+        class="container text-black text-center text-black"
       >
-        <h2 class="font-barlow font-bold mb-2 mt-16 text-6xl">{{$page.blogPage.title}}</h2>
-        <span class="font-barlow text-md font-bolder text-gray-700 text-lg block mb-12">{{$page.blogPage.date}}</span>
-        <div class="text-center mb-10">
-            <g-image class="inline w-full rounded" :src="$page.blogPage.image.src" />
+        <div class="grid grid-cols-4">
+          <div class="col-span-3 bg-gray-900 rounded py-20" ><g-image class="inline rounded max-h-96" :src="$page.texture.image" /></div>
+          <div class="px-6 text-left">
+            <h2 class="font-barlow font-bold mt-5 text-3xl">{{$page.texture.title}}</h2>
+            <div class="mb-2">by <a href="#">njbrown</a></div>
+            <div class="font-inter mb-8 text-lg">{{$page.texture.description}}</div>
+
+            <div class="tags my-8">
+              <span class="px-2 py-2 rounded border-2 border-gray-400 text-gray-600 mr-1">metal</span>
+              <span class="px-2 py-2 rounded border-2 border-gray-400 text-gray-600 mr-1">hexagons</span>
+              <span class="px-2 py-2 rounded border-2 border-gray-400 text-gray-600 mr-1">tile</span>
+            </div>
+
+            <div class="downloads relative bottom-0">
+              <a href="#" class="w-full block px-2 py-2 mt-2 text-white font-bolder rounded-md text-center text-lg bg-blue-900">Download</a>
+              <div class="absolute w-full rounded">
+                <g-link class="w-full block px-2 py-2 text-white font-bolder text-center text-lg bg-blue-900">Zip</g-link>
+                <g-link class="w-full block px-2 py-2 text-white font-bolder text-center text-lg bg-blue-900">Unity</g-link>
+                <g-link class="w-full block px-2 py-2 text-white font-bolder text-center text-lg bg-blue-900">TextureLab (.texture)</g-link>
+              </div>
+            </div>
+
+          </div>
         </div>
-        <!-- <div class="content" v-html="$page.blogPage.content" /> -->
-        <div id="blog-content" class="text-left">
-            <VueRemarkContent />
+
+        <h3 class="font-bold text-3xl mt-12 mb-8">Maps</h3>
+
+        <div class="grid grid-cols-5 gap-3">
+          <div class="relative map-card">
+            <g-image class="inline rounded" src="https://cgbookcase.b-cdn.net/textures/thumbnails/BlackTiles07_1K/BlackTiles07_1K_BaseColor.png" />
+            <div class="map-card-title font-bold text-xl absolute bottom-0 text-white text-center w-full py-4">
+              <span class="bg-gray-900 px-4 py-2 rounded">Albedo</span>
+            </div>
+
+            <div class="w-full hidden grid h-full map-card-contents absolute top-0 items-center justify-center content-center flex">
+              <span class="bg-gray-900 text-white font-bold px-8 py-4 rounded cursor-pointer">Download</span>
+              <span class="bg-gray-900 text-white font-bold px-8 py-4 rounded cursor-pointer mt-2">Copy to Clipboard</span>
+            </div>
+            <!-- <g-link class="w-full block px-2 py-2 mt-2 text-white font-bolder rounded-md text-center text-lg bg-blue-900">Download</g-link> -->
+          </div>
+          
         </div>
+
+        <h3 class="font-bold text-2xl mt-12 mb-8">Similar Textures</h3>
+
       </div>
     </div>
   </Layout>
 </template>
 
 <page-query>
-query Blog ($path: String!) {
-	blogPage: blogPage (path: $path) {
-		title
-		date (format: "MMMM DD, YYYY")
-		image
-		description
-		path
-		content
+query Texture ($path: String!) {
+	texture: galleryPage (path: $path) {
+		id
+    title
+    path
+    image
+    thumbnail
+    description
+    date (format: "MMMM DD, YYYY")
 	}
 }
 </page-query>
@@ -42,9 +79,15 @@ export default {
     OnThisPage,
     NextPrevLinks
   },
+  data:function()
+  {
+    return {
+
+    }
+  },
   metaInfo() {
-    const title = this.$page.blogPage.title;
-    const description = this.$page.blogPage.description || this.$page.blogPage.excerpt;
+    const title = this.$page.texture.title;
+    const description = this.$page.texture.description;
 
     return {
       title: title,
@@ -87,4 +130,13 @@ export default {
 #blog-content {
     @apply mb-2
 }
+
+.map-card:hover > .map-card-contents{
+  display:grid;
+}
+
+.map-card:hover > .map-card-title{
+  display:none;
+}
+
 </style>
